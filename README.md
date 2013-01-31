05b75d4d7771d7fd964815f07cd80ea9b745ead0
Polly Hwu <pollyhwu@gmail.com>
2013-01-31 02:23:29 -0500

    Application setup

--- README.md
+++
@@ -1,4 +0,0 @@
-UF-Happy-Hour
-=============
-
-Senior Project
---
+++ uf-happy-hour/gen/com/example/uf_happy_hour/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.uf_happy_hour;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
